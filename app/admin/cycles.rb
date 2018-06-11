00af349bdd7443365f4a0c6afabd7bd517788de1
Raghu Betina <raghu@firstdraft.com>
2018-06-11 02:30:10 +0000

    Whitelist columns in Cycle for ActiveAdmin

--- app/admin/cycles.rb
+++ app/admin/cycles.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Cycle do
+
+ permit_params :user_id, :begin, :end, :length, :notes
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
